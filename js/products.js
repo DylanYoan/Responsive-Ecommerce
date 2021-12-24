7882fd8008a05c00a1c75569d3208c1c2d147244
Dylan Yoan Martínez Casas <77932027+DylanYoan@users.noreply.github.com>
2021-12-24 13:30:25 -0600

    Update products.js

--- js/products.js
+++ js/products.js
@@ -5,7 +5,7 @@ Category Products
 */
 const getProducts = async ()=>{
     try {
-        const results = await fetch('data/products.json');
+        const results = await fetch('/data/products.json');
         const data = await results.json();
         const products = data.products;
         return products;
